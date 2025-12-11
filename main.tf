678f915d410af83cfc83192cfd3bae13e413b349
BLYY123 <libinglin2014@gmail.com>
2025-12-11 11:31:49 -0800

    find free tier and user first one in terraform file

--- main.tf
+++ main.tf
@@ -2,10 +2,23 @@ provider "aws" {
     region = "us-east-2"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-00e428798e77d38d9"
-  instance_type = "t3.micro"
-  tags = {
-      Name = "TF-Instance"
+data "aws_ec2_instance_types" "free_tier" {
+  filter {
+    name   = "free-tier-eligible"
+    values = ["true"]
   }
 }
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-00e428798e77d38d9"
+  instance_type = data.aws_ec2_instance_types.free_tier.instance_types[0] # Use the first one found
+}
+
+#resource "aws_instance" "foo" {
+#  ami           = "ami-00e428798e77d38d9"
+#  instance_type = "t3.micro"
+#  tags = {
+#      Name = "TF-Instance"
+#  }
+#}
+
