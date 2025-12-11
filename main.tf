ac80bf02529904e735a36ad81d63ac348090cbcc
BLYY123 <libinglin2014@gmail.com>
2025-12-11 11:40:47 -0800

    fix in main.tf

--- main.tf
+++ main.tf
@@ -11,14 +11,9 @@ data "aws_ec2_instance_types" "free_tier" {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-00e428798e77d38d9"
-  instance_type = data.aws_ec2_instance_types.free_tier.instance_types[0] # Use the first one found
+  instance_type = data.aws_ec2_instance_types.free_tier.instance_types[0]
+  tags = {
+      Name = "TF-Instance"
+  }
 }
-
-#resource "aws_instance" "foo" {
-#  ami           = "ami-00e428798e77d38d9"
-#  instance_type = "t3.micro"
-#  tags = {
-#      Name = "TF-Instance"
-#  }
-#}
 
